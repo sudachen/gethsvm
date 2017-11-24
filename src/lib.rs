8a2346ed8f68828e5b3992dc23fd265250c6e31e
Alexey Sudachen <alexey@sudachen.name>
2017-11-24 09:08:09 -0300

    added account's getters

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,14 @@ extern crate libc;
 extern crate block_core;
 extern crate block;
 
-use libc::{c_void,c_char};
-
-use std::mem;
+use std::mem::{drop,forget};
 use std::ffi::{CStr,CString};
 use std::rc::Rc;
 use std::ptr;
 use std::str::FromStr;
 
-use bigint::{Gas, M256, U256, H256, Address};
+use libc::{c_char,size_t};
+use bigint::{Gas, H160, U256, H256, M256, Address};
 use hexutil::*;
 use block::TransactionAction;
 use sputnikvm::errors::RequireError;
@@ -37,11 +36,33 @@ fn u256_from_ptr( ptr: *const c_char ) -> U256 {
     U256::from_str(unsafe{CStr::from_ptr(ptr)}.to_str().unwrap()).unwrap()
 }
 
+fn h256_from_ptr( ptr: *const c_char ) -> H256 {
+    H256::from_str(unsafe{CStr::from_ptr(ptr)}.to_str().unwrap()).unwrap()
+}
+
+fn m256_from_ptr( ptr: *const c_char ) -> M256 {
+    M256::from_str(unsafe{CStr::from_ptr(ptr)}.to_str().unwrap()).unwrap()
+}
+
+struct Accounts {
+    list : Vec<*const AccountChange>,
+    index: usize
+}
+
+impl Accounts {
+    fn next(&mut self) -> Option<*const AccountChange> {
+        if self.index < self.list.len() { self.index+=1; Some(self.list[self.index])} else {None}
+    }
+}
+
 pub struct EvmContext {
     vm : Box<VM>,
     req_blocknum : u64,
     req_address : CString,
     req_hash : CString,
+    out : CString,
+    out_len : usize,
+    account : Accounts
 }
 
 #[no_mangle]
@@ -90,14 +111,26 @@ pub extern fn sputnikvm_context(
         req_blocknum : 0,
         req_address : CString::new("").unwrap(),
         req_hash : CString::new("").unwrap(),
+        out : CString::new("").unwrap(),
+        out_len : 0,
+        account: Accounts{list: Vec::new(), index: 0},
     });
 
     let ptr: *mut _ = &mut *ctx;
-    ::std::mem::forget(ctx);
+    forget(ctx);
     ptr
 }
 
-const SPUTNIK_VM_EXITED : i32 = 0;
+#[no_mangle]
+pub extern fn sputnikvm_terminate(ptr: *mut EvmContext) {
+    drop(ptr)
+}
+
+const SPUTNIK_VM_EXITED_OK : i32 = 0;
+const SPUTNIK_VM_EXITED_ERR : i32 = 1;
+const SPUTNIK_VM_RUNNING : i32 = 2;
+const SPUTNIK_VM_UNSUPPORTED_ERR : i32 = 3;
+
 const SPUTNIK_VM_REQUIRE_ACCOUNT : i32 = 2;
 const SPUTNIK_VM_REQUIRE_CODE : i32 = 3;
 const SPUTNIK_VM_REQUIRE_HASH : i32 = 4;
@@ -124,7 +157,7 @@ pub extern fn sputnikvm_fire(ptr: *mut EvmContext) -> i32 {
             ctx.req_blocknum = number.as_u64();
             SPUTNIK_VM_REQUIRE_HASH
         },
-        Ok(()) => SPUTNIK_VM_EXITED,
+        Ok(()) => SPUTNIK_VM_EXITED_OK,
     }
 }
 
@@ -199,4 +232,182 @@ pub extern fn sputnikvm_commit_code(
         address: address_from_ptr(addr_ptr),
         code: Rc::new(code),
     });
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_commit_blockhash(
+    ptr: *mut EvmContext,
+    number: u64,
+    hash_ptr: *const c_char) {
+
+    let ctx = unsafe{&mut *ptr};
+    ctx.vm.commit_blockhash(number.into(), h256_from_ptr(hash_ptr));
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_commit_value(
+    ptr: *mut EvmContext,
+    addr_ptr: *const c_char,
+    key_ptr: *const c_char,
+    value_ptr: *const c_char) {
+
+    let ctx = unsafe{&mut *ptr};
+    ctx.vm.commit_account(AccountCommitment::Storage {
+        address: address_from_ptr(addr_ptr),
+        index: u256_from_ptr(key_ptr),
+        value: m256_from_ptr(value_ptr),
+    });
+
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_out(ptr: *mut EvmContext) -> *const c_char {
+    let ctx = unsafe{&mut *ptr};
+    let v = ctx.vm.out().to_vec();
+    ctx.out_len = v.len();
+    ctx.out = unsafe { CString::from_vec_unchecked(v) };
+    ctx.out.as_ptr()
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_out_len(ptr: *mut EvmContext) -> size_t {
+    let ctx = unsafe{&mut *ptr};
+    ctx.out_len
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_gas(ptr: *mut EvmContext) -> *const c_char {
+    let ctx = unsafe{&mut *ptr};
+    ctx.out = CString::new(format!("0x{:x}",ctx.vm.available_gas())).unwrap();
+    ctx.out.as_ptr()
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_refund(ptr: *mut EvmContext) -> *const c_char {
+    let ctx = unsafe{&mut *ptr};
+    ctx.out = CString::new(format!("0x{:x}",ctx.vm.refunded_gas())).unwrap();
+    ctx.out.as_ptr()
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_status(ptr: *mut EvmContext) -> i32 {
+    let ctx = unsafe{&mut *ptr};
+    match ctx.vm.status() {
+        VMStatus::Running => SPUTNIK_VM_RUNNING,
+        VMStatus::ExitedOk => SPUTNIK_VM_EXITED_OK,
+        VMStatus::ExitedErr(_) => SPUTNIK_VM_EXITED_ERR,
+        VMStatus::ExitedNotSupported(_) => SPUTNIK_VM_UNSUPPORTED_ERR,
+    }
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_error(ptr: *mut EvmContext) -> *const c_char {
+    let ctx = unsafe{&mut *ptr};
+    match ctx.vm.status() {
+        VMStatus::Running => ptr::null(),
+        VMStatus::ExitedOk => ptr::null(),
+        VMStatus::ExitedErr(e) => ptr::null(),//(ctx.error = CString::new(e)).as_ptr(),
+        VMStatus::ExitedNotSupported(e) => ptr::null(),//(ctx.error = CString::new(e)).as_ptr(),
+    }
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_first_account(ptr: *mut EvmContext) -> *const AccountChange {
+    let ctx = unsafe{&mut *ptr};
+    ctx.account =
+        Accounts{
+            list: ctx.vm.accounts().into_iter().map(|c|{let x: *const AccountChange = c; x}).collect(),
+            index: 0,
+        };
+    match ctx.account.next() {
+        None => ptr::null(),
+        Some(x) => x
+    }
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_next_account(ptr: *mut EvmContext) -> *const AccountChange {
+    let ctx = unsafe{&mut *ptr};
+    match ctx.account.next() {
+        None => ptr::null(),
+        Some(x) => x
+    }
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_acc_address(q: *mut EvmContext, ptr: *const AccountChange) -> *const c_char {
+    let ctx = unsafe{&mut *q};
+    let acc = unsafe{&*ptr};
+    let address = match acc {
+        &AccountChange::Full {ref address, .. } => address,
+        &AccountChange::Create {ref address, .. } => address,
+        &AccountChange::IncreaseBalance (ref address, _) => address,
+        &AccountChange::DecreaseBalance (ref address, _) => address,
+    };
+    ctx.out = CString::new(format!("0x{:x}",address)).unwrap();
+    ctx.out.as_ptr()
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_acc_balance(q: *mut EvmContext, ptr: *const AccountChange) -> *const c_char {
+    let ctx = unsafe{&mut *q};
+    let acc = unsafe{&*ptr};
+    let balance = match acc {
+        &AccountChange::Full {ref balance, .. } => balance,
+        &AccountChange::Create {ref balance, ..} => balance,
+        &AccountChange::IncreaseBalance (_, ref balance) => balance,
+        &AccountChange::DecreaseBalance (_, ref balance) => balance,
+    };
+    ctx.out = CString::new(format!("0x{:x}",balance)).unwrap();
+    ctx.out.as_ptr()
+}
+
+const UPDATE_ACCOUNT : i32 = 0;
+const CREATE_ACCOUNT : i32 = 1;
+const SUICIDE_ACCOUNT : i32 = 2;
+const ADD_BALANCE_ACCOUNT : i32 = 3;
+const SUB_BALANCE_ACCOUNT : i32 = 4;
+
+#[no_mangle]
+pub extern fn sputnikvm_acc_change(ptr: *const AccountChange) -> i32 {
+    let acc = unsafe{&*ptr};
+    match acc {
+        &AccountChange::Full {..} => UPDATE_ACCOUNT,
+        &AccountChange::Create {..} => CREATE_ACCOUNT,
+        &AccountChange::IncreaseBalance {..} => ADD_BALANCE_ACCOUNT,
+        &AccountChange::DecreaseBalance {..} => SUB_BALANCE_ACCOUNT,
+    }
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_acc_nonce(ptr: *const AccountChange) -> u64 {
+    let acc = unsafe{&*ptr};
+    match acc {
+        &AccountChange::Full {nonce, ..} => nonce.into(),
+        &AccountChange::Create {nonce, ..} => nonce.into(),
+        _ => 0,
+    }
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_acc_code(q: *mut EvmContext, ptr: *const AccountChange) -> *const c_char {
+    let ctx = unsafe{&mut *q};
+    let acc = unsafe{&*ptr};
+    let bytes = match acc {
+        &AccountChange::Full {ref code, ..} => Rc::clone(code),
+        &AccountChange::Create {ref code, ..} => Rc::clone(code),
+        _ => Rc::new(EMPTY.as_ref().into()),
+    };
+    ctx.out = unsafe { CString::from_vec_unchecked(bytes.to_vec()) };
+    ctx.out.as_ptr()
+}
+
+#[no_mangle]
+pub extern fn sputnikvm_acc_code_len(ptr: *const AccountChange) -> size_t {
+    let acc = unsafe{&*ptr};
+    match acc {
+        &AccountChange::Full {ref code, ..} => code.len(),
+        &AccountChange::Create {ref code, ..} => code.len(),
+        _ => 0,
+    }
 }
