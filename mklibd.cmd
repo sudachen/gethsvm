b0196b0eec3b19b88df83607e48823c695a17a34
Alexey Sudachen <alexey@sudachen.name>
2017-12-04 01:30:39 -0300

    library name fixed to sputnikvm

--- mklibd.cmd
+++ mklibd.cmd
@@ -2,5 +2,5 @@ cd %~dp0
 cargo build
 cd %GOPATH%\src\github.com\ethereumproject\go-ethereum\machine\sputnik\windows_amd64
 del /q libsputnikvm.a
-copy %~dp0target\debug\svmffi.lib libsputnikvm.a
+copy %~dp0target\debug\sputnikvm.lib libsputnikvm.a
 
--- mklibr.cmd
+++ mklibr.cmd
@@ -2,5 +2,5 @@ cd %~dp0
 cargo build --release
 cd %GOPATH%\src\github.com\ethereumproject\go-ethereum\machine\sputnik\windows_amd64
 del /q libsputnikvm.a
-copy %~dp0target\release\svmffi.lib libsputnikvm.a
+copy %~dp0target\release\sputnikvm.lib libsputnikvm.a
 
